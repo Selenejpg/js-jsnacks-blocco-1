60f83198644c8a36f0c8f0e8796d3a068cbff7ce
noemipintaldi <noemipintaldi@gmail.com>
2022-03-16 15:35:58 +0100

    JSnack 3 completato

--- JSnack 3/myscript.js
+++ JSnack 3/myscript.js
@@ -1,10 +1,10 @@
 let sommaFor = 0;
 
 for (let i = 0; i < 10; i++) {
-    const nuovoNumero = parseInt(prompt("inserisci un numero"));
+    const nuovoNumero = parseInt(prompt(`inserisci un numero`));
     sommaFor += nuovoNumero;
 }
 
-document.getElementById("somma") = `"La somma dei numeri è:" + ${sommaFor}`
+document.getElementById("somma").innerHTML = `La somma dei numeri è: ` + sommaFor;
 
 
