a5a9bb10a26d6b41ce862ed59fba64dede887a9a
noemipintaldi <noemipintaldi@gmail.com>
2022-03-16 13:07:39 +0100

    push incompleto

--- JSnack 2/myscript.js
+++ JSnack 2/myscript.js
@@ -6,6 +6,8 @@ let parolaDue = prompt ("Scrivi la seconda parola").length;
 
 if ( parolaUno > parolaDue) {
     document.getElementById("parolaLunga").innerHTML = `${parolaUno}`;
+    document.getElementById("parolaCorta").innerHTML = `${parolaDue}`;
 }else {
     document.getElementById("parolaLunga").innerHTML = `${parolaDue}`;
+    document.getElementById("parolaCorta").innerHTML = `${parolaUno}`;
 }
---
+++ JSnack 3/myscript.js
@@ -0,0 +1,10 @@
+let sommaFor = 0;
+
+for (let i = 0; i < 10; i++) {
+    const nuovoNumero = parseInt(prompt("inserisci un numero"));
+    sommaFor += nuovoNumero;
+}
+
+document.getElementById("somma") = `"La somma dei numeri è:" + ${sommaFor}`
+
+
