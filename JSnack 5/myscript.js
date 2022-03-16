6f15ddb97cd8a60083840ceb8f7a7435eea821d4
noemipintaldi <noemipintaldi@gmail.com>
2022-03-16 23:50:58 +0100

    snack 5 incompleto

---
+++ JSnack 5/myscript.js
@@ -0,0 +1,15 @@
+//Crea un array vuoto.
+let array = [];
+let boh = "";
+
+//Chiedi per 6 volte all’utente di inserire un numero,
+for (let i = 0; i < 6; i++) {
+    let boh = prompt("Inserisci un numero"); 
+}
+
+//Se è dispari inseriscilo nell’array
+if (boh % 2 == 0) {
+    
+}else {
+    array.push(boh);
+}
