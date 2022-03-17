1ee599c9bba6cbf86e9ffc85362ce0ca4d81f172
noemipintaldi <noemipintaldi@gmail.com>
2022-03-17 10:59:47 +0100

    snack 5 corretto

--- JSnack 5/myscript.js
+++ JSnack 5/myscript.js
@@ -1,15 +1,16 @@
 //Crea un array vuoto.
 let array = [];
-let boh = "";
 
 //Chiedi per 6 volte all’utente di inserire un numero,
 for (let i = 0; i < 6; i++) {
-    let boh = prompt("Inserisci un numero"); 
+    const numeroRichiesto = parseInt(prompt("Inserisci un numero"));
+
+    //Se è dispari inseriscilo nell’array
+    if (numeroRichiesto % 2 != 0) {
+        array.push(numeroRichiesto);
+    }
 }
 
-//Se è dispari inseriscilo nell’array
-if (boh % 2 == 0) {
-    
-}else {
-    array.push(boh);
-}
+console.log(array);
+
+
